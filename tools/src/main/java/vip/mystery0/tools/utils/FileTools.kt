694cd8c4d07bc12c5127443e25b4162ab58d363e
Mystery00 <mystery0dyl520@gmail.com>
2018-03-23 23:12:00 +0800

    add get path from uri

--- tools/src/main/java/vip/mystery0/tools/utils/FileTools.kt
+++ tools/src/main/java/vip/mystery0/tools/utils/FileTools.kt
@@ -17,11 +17,87 @@
 
 package vip.mystery0.tools.utils
 
+import android.content.ContentUris
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.support.annotation.RequiresApi
 import java.io.File
 import java.text.DecimalFormat
 
 open class FileTools {
 	companion object {
+		@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+		@JvmStatic
+		fun getPath(context: Context, uri: Uri): String? {
+			when {
+				DocumentsContract.isDocumentUri(context, uri) -> {
+					when (uri.authority) {
+						"com.android.externalstorage.documents" -> {
+							val docId = DocumentsContract.getDocumentId(uri)
+							val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+							val type = split[0]
+
+							if ("primary".equals(type, ignoreCase = true)) {
+								return Environment.getExternalStorageDirectory().toString() + File.pathSeparator + split[1]
+							}
+						}
+						"com.android.providers.downloads.documents" -> {
+							val id = DocumentsContract.getDocumentId(uri)
+							val contentUri = ContentUris.withAppendedId(
+									Uri.parse("content://downloads/public_downloads"), id.toLong())
+
+							return getDataColumn(context, contentUri, null, null)
+						}
+						"com.android.providers.media.documents" -> {
+							val docId = DocumentsContract.getDocumentId(uri)
+							val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+							val type = split[0]
+
+							var contentUri: Uri? = null
+							when (type) {
+								"image" -> contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+								"video" -> contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+								"audio" -> contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+							}
+
+							val selection = "_id=?"
+							val selectionArgs = arrayOf(split[1])
+
+							return getDataColumn(context, contentUri!!, selection, selectionArgs)
+						}
+					}
+				}
+				"content".equals(uri.scheme, ignoreCase = true) ->
+					return getDataColumn(context, uri, null, null)
+				"file".equals(uri.scheme, ignoreCase = true) ->
+					return uri.path
+			}
+			return null
+		}
+
+		private fun getDataColumn(context: Context, uri: Uri, selection: String?,
+								  selectionArgs: Array<String>?): String? {
+			var cursor: Cursor? = null
+			val column = "_data"
+			val projection = arrayOf(column)
+			try {
+				cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+				if (cursor != null && cursor.moveToFirst()) {
+					val columnIndex = cursor.getColumnIndexOrThrow(column)
+					return cursor.getString(columnIndex)
+				}
+			} finally {
+				if (cursor != null)
+					cursor.close()
+			}
+			return null
+		}
+
 		@JvmStatic
 		fun formatFileSize(file: File, decimalNum: Int = 2): String {
 			return if (!file.exists()) "0B" else formatFileSize(file.length(), decimalNum)
@@ -35,14 +111,12 @@ open class FileTools {
 				formatString.append('0')
 			val decimalFormat = DecimalFormat(formatString.toString())
 			val fileSizeString: String
-			if (fileSize < 1024)
-				fileSizeString = decimalFormat.format(fileSize) + 'B'
-			else if (fileSize < 1024 * 1024)
-				fileSizeString = decimalFormat.format(fileSize / 1024) + "KB"
-			else if (fileSize < 1024 * 1024 * 1024)
-				fileSizeString = decimalFormat.format(fileSize / 1024 / 1024) + "MB"
-			else
-				fileSizeString = decimalFormat.format(fileSize / 1024 / 1024 / 1024) + "GB"
+			fileSizeString = when {
+				fileSize < 1024 -> decimalFormat.format(fileSize) + 'B'
+				fileSize < 1024 * 1024 -> decimalFormat.format(fileSize / 1024) + "KB"
+				fileSize < 1024 * 1024 * 1024 -> decimalFormat.format(fileSize / 1024 / 1024) + "MB"
+				else -> decimalFormat.format(fileSize / 1024 / 1024 / 1024) + "GB"
+			}
 			return fileSizeString
 		}
 	}
