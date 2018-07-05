9e9263c0fe11dc8f72125e90a3fbf63c9e3f9634
Mystery0 <mystery0dyl520@gmail.com>
2018-07-05 16:05:17 +0800

    add killProcess when the process execute time too long

--- app/src/main/java/vip/mystery0/simple_tools/MainActivity.java
+++ app/src/main/java/vip/mystery0/simple_tools/MainActivity.java
@@ -3,7 +3,7 @@ package vip.mystery0.simple_tools;
 import android.util.Log;
 
 import vip.mystery0.tools.base.BaseActivity;
-import vip.mystery0.tools.utils.CmdTools;
+import vip.mystery0.tools.utils.CommandTools;
 
 public class MainActivity extends BaseActivity {
 	private static final String TAG = "MainActivity";
@@ -20,16 +20,30 @@ public class MainActivity extends BaseActivity {
 	@Override
 	public void initData() {
 		super.initData();
-		Log.i(TAG, "onCreate: " + CmdTools.INSTANCE.isRoot());
-		Log.i(TAG, "onCreate: " + CmdTools.INSTANCE.requestSU());
+		Log.i(TAG, "initData: " + CommandTools.INSTANCE.isRoot());
+		Log.i(TAG, "initData: " + CommandTools.INSTANCE.requestSU());
 		String[] cmds = new String[]{"echo test1", "echo test2", "echo test3"};
-		CmdTools.CommandResult result1 = CmdTools.INSTANCE.execCommands(cmds);
-		Log.i(TAG, "onCreate: " + result1);
+		CommandTools.CommandResult result1 = CommandTools.INSTANCE.execCommands(cmds);
+		Log.i(TAG, "initData: " + result1);
 		String[] cmdss = new String[]{"ls", "cd /sdcard/", "ls"};
-		CmdTools.CommandResult result2 = CmdTools.INSTANCE.execRootCommands(cmdss);
-		Log.i(TAG, "onCreate: " + result2);
-		CmdTools.CommandResult result3 = CmdTools.INSTANCE.execRootCommand("ls /data");
-		Log.i(TAG, "onCreate: " + result3);
+		CommandTools.CommandResult result2 = CommandTools.INSTANCE.execRootCommands(cmdss);
+		Log.i(TAG, "initData: " + result2);
+		CommandTools.CommandResult result3 = CommandTools.INSTANCE.execRootCommand("ls /data");
+		Log.i(TAG, "initData: " + result3);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				CommandTools.CommandResult result4 = CommandTools.INSTANCE.execCommand("ping 127.0.0.1");
+				Log.i(TAG, "initData: " + result4);
+			}
+		}).start();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		CommandTools.INSTANCE.killProcess();
+		Log.i(TAG, "initData: kill");
 	}
 
 	@Override
