0c8707d162507eebd4d0e2d869f88c86755aa763
Mystery0 <mystery0dyl520@gmail.com>
2018-08-08 14:54:17 +0800

    update base RecyclerViewAdapter

--- tools/src/main/java/vip/mystery0/tools/base/BaseRecyclerViewAdapter.kt
+++ tools/src/main/java/vip/mystery0/tools/base/BaseRecyclerViewAdapter.kt
@@ -1,24 +1,21 @@
 package vip.mystery0.tools.base
 
-import android.content.Context
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-abstract class BaseRecyclerViewAdapter<T : RecyclerView.ViewHolder, in C : Any>
-(private val context: Context?,
- @LayoutRes private val itemLayoutId: Int,
- private val list: ArrayList<in C>) : RecyclerView.Adapter<T>() {
+abstract class BaseRecyclerViewAdapter<T : RecyclerView.ViewHolder, M : Any>(@LayoutRes private val itemLayoutId: Int) : RecyclerView.Adapter<T>() {
+	val list = ArrayList<M>()
+
 	override fun getItemCount(): Int = list.size
 
 	override fun onBindViewHolder(holder: T, position: Int) {
-		@Suppress("UNCHECKED_CAST")
-		setItemView(holder, position, list[position] as C)
+		setItemView(holder, position, list[position])
 	}
 
-	abstract fun setItemView(holder: T, position: Int, data: C)
+	abstract fun setItemView(holder: T, position: Int, data: M)
 
-	fun createView(parent: ViewGroup): View = LayoutInflater.from(context).inflate(itemLayoutId, parent, false)
+	fun createView(parent: ViewGroup): View = LayoutInflater.from(parent.context).inflate(itemLayoutId, parent, false)
 }
