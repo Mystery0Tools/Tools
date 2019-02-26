d4478252b4374a129f4cadf932341d9462dd6d6a
Mystery0 <mystery0dyl520@gmail.com>
2019-02-26 22:43:47 +0800

    添加Cookie管理器

---
+++ tools/src/main/java/vip/mystery0/cookie/LoadCookiesInterceptor.kt
@@ -0,0 +1,15 @@
+package vip.mystery0.cookie
+
+import android.text.TextUtils
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class LoadCookiesInterceptor : Interceptor {
+	override fun intercept(chain: Interceptor.Chain): Response {
+		val request = chain.request()
+		val builder = request.newBuilder()
+		val cookie = CookieManager.getCookie(request.url().host())
+		if (!TextUtils.isEmpty(cookie)) builder.addHeader("Cookie", cookie!!)
+		return chain.proceed(builder.build())
+	}
+}
