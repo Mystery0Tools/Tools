888bacedd4496a76fba403cf6440db38e2ec414f
Mystery0 <mystery0dyl520@gmail.com>
2019-05-28 14:19:11 +0800

    feat: 添加时间工具类

---
+++ tools/src/main/java/vip/mystery0/tools/utils/TimeTools.kt
@@ -0,0 +1,39 @@
+package vip.mystery0.tools.utils
+
+object TimeTools {
+	/**
+	 * 毫秒转化时分秒毫秒
+	 * @param ms
+	 * @return
+	 */
+	fun formatTime(ms: Long?): String {
+		val ss = 1000
+		val mi = ss * 60
+		val hh = mi * 60
+		val dd = hh * 24
+
+		val day = ms!! / dd
+		val hour = (ms - day * dd) / hh
+		val minute = (ms - day * dd - hour * hh) / mi
+		val second = (ms - day * dd - hour * hh - minute * mi) / ss
+		val milliSecond = ms - day * dd - hour * hh - minute * mi - second * ss
+
+		val sb = StringBuffer()
+		if (day > 0) {
+			sb.append(day.toString() + "天")
+		}
+		if (hour > 0) {
+			sb.append(hour.toString() + "小时")
+		}
+		if (minute > 0) {
+			sb.append(minute.toString() + "分")
+		}
+		if (second > 0) {
+			sb.append(second.toString() + "秒")
+		}
+		if (milliSecond > 0) {
+			sb.append(milliSecond.toString() + "毫秒")
+		}
+		return sb.toString()
+	}
+}
