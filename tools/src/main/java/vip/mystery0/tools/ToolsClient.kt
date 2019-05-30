f13e31c516381778c91107620310b368b29edc51
Mystery0 <mystery0dyl520@gmail.com>
2019-05-30 20:24:53 +0800

    feat: 添加获取array数组的方法

--- tools/src/main/java/vip/mystery0/tools/ToolsClient.kt
+++ tools/src/main/java/vip/mystery0/tools/ToolsClient.kt
@@ -3,6 +3,7 @@ package vip.mystery0.tools
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
+import androidx.annotation.ArrayRes
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
@@ -10,6 +11,8 @@ import androidx.core.content.ContextCompat
 fun Context.getTColor(@ColorRes resId: Int): Int = ContextCompat.getColor(this, resId)
 
 fun Context.getTDrawable(@DrawableRes resId: Int): Drawable? = ContextCompat.getDrawable(this, resId)
+
+fun Context.getTStringArray(@ArrayRes resId: Int): Array<String> = this.resources.getStringArray(resId)
 
 @SuppressLint("StaticFieldLeak")
 object ToolsClient {
