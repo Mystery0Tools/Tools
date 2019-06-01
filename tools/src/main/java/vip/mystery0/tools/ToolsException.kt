a62bde2658660373056da61a44a0c888a8a31493
Mystery0 <mystery0dyl520@gmail.com>
2019-06-01 22:24:55 +0800

    feat: 添加是否忽略异常的参数

--- tools/src/main/java/vip/mystery0/tools/ToolsException.kt
+++ tools/src/main/java/vip/mystery0/tools/ToolsException.kt
@@ -4,9 +4,10 @@ open class ToolsException(val code: Int, override val message: String?) : Runtim
 	companion object {
 		const val ERROR = 101
 		const val FILE_NOT_EXIST = 102
-		const val MAKE_DIR_ERROR = 103
-		const val NOT_FILE = 104
-		const val NOT_DIRECTORY = 105
+		const val FILE_EXIST = 103
+		const val MAKE_DIR_ERROR = 104
+		const val NOT_FILE = 105
+		const val NOT_DIRECTORY = 106
 	}
 
 	constructor(e: Throwable) : this(ERROR, e.message)
