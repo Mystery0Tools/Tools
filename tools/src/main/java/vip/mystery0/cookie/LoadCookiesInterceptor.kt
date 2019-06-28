097174578e66f3c916ed327cc21572ccf4bef495
Mystery0 <mystery0dyl520@gmail.com>
2019-06-28 15:46:20 +0800

    fix: 修复编译失败

--- tools/src/main/java/vip/mystery0/cookie/LoadCookiesInterceptor.kt
+++ tools/src/main/java/vip/mystery0/cookie/LoadCookiesInterceptor.kt
@@ -8,7 +8,7 @@ class LoadCookiesInterceptor : Interceptor {
 	override fun intercept(chain: Interceptor.Chain): Response {
 		val request = chain.request()
 		val builder = request.newBuilder()
-		val cookie = CookieManager.instance.getCookie(request.url().host())
+		val cookie = CookieManager.instance.getCookie(request.url.host)
 		if (!TextUtils.isEmpty(cookie)) builder.addHeader("Cookie", cookie!!)
 		return chain.proceed(builder.build())
 	}
