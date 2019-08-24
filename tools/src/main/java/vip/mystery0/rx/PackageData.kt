ebe6990bcce6bcc718df2042b0aff41614f9fed2
Mystery0 <mystery0dyl520@gmail.com>
2019-08-24 13:44:03 +0800

    feat: PackageData添加包级函数

--- tools/src/main/java/vip/mystery0/rx/PackageData.kt
+++ tools/src/main/java/vip/mystery0/rx/PackageData.kt
@@ -1,15 +1,36 @@
 package vip.mystery0.rx
 
+fun <T> content(data: T?): PackageData<T> = PackageData.content(data)
+fun <T> error(data: T?, error: Throwable?): PackageData<T> = PackageData.error(data, error)
+fun <T> error(error: Throwable?): PackageData<T> = PackageData.error(error)
+fun <T> empty(data: T?): PackageData<T> = PackageData.empty(data)
+fun <T> empty(): PackageData<T> = PackageData.empty()
+fun <T> loading(data: T?): PackageData<T> = PackageData.loading(data)
+fun <T> loading(): PackageData<T> = PackageData.loading()
+
 class PackageData<T>(val status: Status,
 					 val data: T?,
 					 val error: Throwable?) {
 	companion object {
+		@JvmStatic
 		fun <T> content(data: T?): PackageData<T> = PackageData(Status.Content, data, null)
+
+		@JvmStatic
 		fun <T> error(data: T?, error: Throwable?): PackageData<T> = PackageData(Status.Error, data, error)
+
+		@JvmStatic
 		fun <T> error(error: Throwable?): PackageData<T> = PackageData(Status.Error, null, error)
+
+		@JvmStatic
 		fun <T> empty(data: T?): PackageData<T> = PackageData(Status.Empty, data, null)
+
+		@JvmStatic
 		fun <T> empty(): PackageData<T> = PackageData(Status.Empty, null, null)
+
+		@JvmStatic
 		fun <T> loading(data: T?): PackageData<T> = PackageData(Status.Loading, data, null)
+
+		@JvmStatic
 		fun <T> loading(): PackageData<T> = PackageData(Status.Loading, null, null)
 	}
 }
