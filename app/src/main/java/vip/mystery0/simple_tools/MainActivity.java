87f28a4e535e9a46205e43d9719b2b1220142ef1
Mystery0 <mystery0dyl520@gmail.com>
2019-08-24 15:21:57 +0800

    fix: 修复PackageData观察者无法使用

--- app/src/main/java/vip/mystery0/simple_tools/MainActivity.java
+++ app/src/main/java/vip/mystery0/simple_tools/MainActivity.java
@@ -1,17 +1,12 @@
 package vip.mystery0.simple_tools;
 
-import android.util.Log;
+import androidx.lifecycle.MutableLiveData;
 
-import java.io.File;
-
+import vip.mystery0.rx.PackageData;
+import vip.mystery0.rx.PackageDataObserver;
 import vip.mystery0.tools.base.BaseActivity;
-import vip.mystery0.tools.utils.ActivityManagerTools;
-import vip.mystery0.tools.utils.ArchiveTools;
-import vip.mystery0.tools.utils.CommandTools;
-import vip.mystery0.view.ProgressDialog;
 
 public class MainActivity extends BaseActivity {
-	private static final String TAG = "MainActivity";
 
 	public MainActivity() {
 		super(R.layout.activity_main);
@@ -25,29 +20,6 @@ public class MainActivity extends BaseActivity {
 	@Override
 	public void initData() {
 		super.initData();
-//		final ProgressDialog progressDialog = new ProgressDialog(this);
-//		progressDialog.setTitle("title");
-//		progressDialog.setMessage("message");
-//		progressDialog.show();
-
-//		new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				ArchiveTools.Companion.tarGz()
-//						.setCompressDir(new File("/sdcard/JanYo Share"))
-//						.setArchiveFileName("test")
-//						.setSavePath(new File("/sdcard/"))
-//						.setSuffix("tar.gz")
-//						.compress();
-
-//				runOnUiThread(new Runnable() {
-//					@Override
-//					public void run() {
-//						progressDialog.cancel();
-//					}
-//				});
-//			}
-//		}).start();
 	}
 
 	@Override
