4eedb48c8a95ec67871435d76e4d5ecc3003c0e2
Mystery0 <mystery0dyl520@gmail.com>
2019-09-25 21:15:57 +0800

    fix: 修复Android Q上检查网络链接错误

--- tools/src/main/java/vip/mystery0/tools/utils/NetworkTools.kt
+++ tools/src/main/java/vip/mystery0/tools/utils/NetworkTools.kt
@@ -2,12 +2,25 @@ package vip.mystery0.tools.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.annotation.RequiresPermission
 import vip.mystery0.tools.context
 
 @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
 fun isConnectInternet(): Boolean {
 	val connectivityManager = context().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-	val activeNetworkInfo = connectivityManager.activeNetworkInfo
-	return activeNetworkInfo != null && activeNetworkInfo.isConnected
+	if (sdkIsBefore(AndroidVersionCode.VERSION_Q)) {
+		val networkInfo = connectivityManager.activeNetworkInfo
+		return networkInfo != null && networkInfo.isConnected
+	}
+	val networks = connectivityManager.allNetworks
+	if (networks.isNotEmpty()) {
+		for (network in networks) {
+			val nc = connectivityManager.getNetworkCapabilities(network)
+			if (nc!!.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+				return true
+			}
+		}
+	}
+	return false
 }
