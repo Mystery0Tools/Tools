e56ddeed878fe3bf6e6b06d3772d79e1c8e15e0e
Mystery0 <mystery0dyl520@gmail.com>
2019-10-03 20:48:16 +0800

    fix: 修改字符串资源的异常类

--- tools/src/main/java/vip/mystery0/tools/ResourceException.kt
+++ tools/src/main/java/vip/mystery0/tools/ResourceException.kt
@@ -5,7 +5,8 @@ import android.widget.Toast
 import androidx.annotation.StringRes
 
 class ResourceException(@StringRes val id: Int) : RuntimeException() {
-	fun message(context: Context): String = context.getString(id)
+	override val message: String?
+		get() = context().getString(id)
 }
 
 fun Throwable?.toast(context: Context = context()) = dispatch(this, false, context)
@@ -13,13 +14,8 @@ fun Throwable?.toastLong(context: Context = context()) = dispatch(this, true, co
 fun String?.toast(context: Context = context()) = newToast(context, this, Toast.LENGTH_SHORT)
 fun String?.toastLong(context: Context = context()) = newToast(context, this, Toast.LENGTH_LONG)
 
-fun Throwable?.dispatchMessage(): String? = when (this) {
-	is ResourceException -> this.message(context())
-	else -> this?.message
-}
-
 private fun dispatch(throwable: Throwable?, isLong: Boolean, context: Context = context()) {
-	newToast(context, throwable.dispatchMessage(), if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT)
+	newToast(context, throwable?.message, if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT)
 }
 
 private fun newToast(context: Context, message: String?, length: Int) {
