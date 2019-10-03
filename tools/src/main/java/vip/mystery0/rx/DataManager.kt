9c409bb7ea2f0ae0d53dc9b087f6278c2474fa91
Mystery0 <mystery0dyl520@gmail.com>
2019-10-03 17:02:07 +0800

    feat: 添加一个ViewModel开启协程的函数

--- tools/src/main/java/vip/mystery0/rx/DataManager.kt
+++ tools/src/main/java/vip/mystery0/rx/DataManager.kt
@@ -1,7 +1,11 @@
 package vip.mystery0.rx
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import java.util.concurrent.Executors
 
 fun <T, D : MutableLiveData<T>> D.doContent(data: T?) = this.postValue(data)
@@ -13,7 +17,12 @@ fun <T, D : MutableLiveData<PackageData<T>>> D.empty() = this.postValue(dataEmpt
 fun <T, D : MutableLiveData<PackageData<T>>> D.loading(data: T?) = this.postValue(dataLoading(data))
 fun <T, D : MutableLiveData<PackageData<T>>> D.loading() = this.postValue(dataLoading())
 
-fun <T> dispatchException(liveData: MutableLiveData<PackageData<T>>): CoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable -> liveData.error(throwable) }
+fun <T> ViewModel.launch(liveData: MutableLiveData<PackageData<T>>,
+						 action: suspend CoroutineScope.() -> Unit) =
+		viewModelScope.launch(dispatchException(liveData)) { action() }
+
+fun <T> dispatchException(liveData: MutableLiveData<PackageData<T>>): CoroutineExceptionHandler =
+		CoroutineExceptionHandler { _, throwable -> liveData.error(throwable) }
 
 class DataManager private constructor(threadNum: Int) {
 	companion object {
