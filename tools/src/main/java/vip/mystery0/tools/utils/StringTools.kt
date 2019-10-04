ebc848727f8d5e7b9758296251e3ba0b61fefc77
Mystery0 <mystery0dyl520@gmail.com>
2019-10-04 23:56:37 +0800

    feat: 添加对字节数组进行加密的算法

--- tools/src/main/java/vip/mystery0/tools/utils/StringTools.kt
+++ tools/src/main/java/vip/mystery0/tools/utils/StringTools.kt
@@ -2,33 +2,39 @@ package vip.mystery0.tools.utils
 
 import java.security.MessageDigest
 
+fun String.md5(): String = toByteArray().md5()
+
 /**
  * MD5 加密
  * @return MD5 加密之后的字符串
  */
-fun String.md5(): String {
+fun ByteArray.md5(): String {
 	val digest = MessageDigest.getInstance("MD5")
-	val result = digest.digest(toByteArray())
+	val result = digest.digest(this)
 	return toHex(result)
 }
+
+fun String.sha1(): String = toByteArray().sha1()
 
 /**
  * SHA-1 加密
  * @return SHA-1 加密之后的字符串
  */
-fun String.sha1(): String {
+fun ByteArray.sha1(): String {
 	val digest = MessageDigest.getInstance("SHA-1")
-	val result = digest.digest(toByteArray())
+	val result = digest.digest(this)
 	return toHex(result)
 }
+
+fun String.sha256(): String = toByteArray().sha256()
 
 /**
  * SHA-256 加密
  * @return SHA-256 加密之后的字符串
  */
-fun String.sha256(): String {
+fun ByteArray.sha256(): String {
 	val digest = MessageDigest.getInstance("SHA-256")
-	val result = digest.digest(toByteArray())
+	val result = digest.digest(this)
 	return toHex(result)
 }
 
