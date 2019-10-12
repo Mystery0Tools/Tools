3f25939784d6ab3529610c5f7201df3c85194e0e
Mystery0 <mystery0dyl520@gmail.com>
2019-10-13 00:20:53 +0800

    feat: 添加获取SharedPreference的函数

--- tools/src/main/java/vip/mystery0/tools/utils/Utility.kt
+++ tools/src/main/java/vip/mystery0/tools/utils/Utility.kt
@@ -1,9 +1,12 @@
 package vip.mystery0.tools.utils
 
 import android.content.SharedPreferences
+import vip.mystery0.tools.context
 
 fun SharedPreferences.use(block: (SharedPreferences.Editor) -> Unit) {
 	val editor = edit()
 	block(editor)
 	editor.apply()
 }
+
+fun sp(name: String, mode: Int): SharedPreferences = context().getSharedPreferences(name, mode)
