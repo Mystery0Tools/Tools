5e603dc2897d9fadcaf2885ae1e3857ea0791b73
Mystery0 <mystery0dyl520@gmail.com>
2020-01-21 15:54:40 +0800

    fix: 移除线程类

--- tools/src/main/java/vip/mystery0/rx/DataManager.kt
+++ tools/src/main/java/vip/mystery0/rx/DataManager.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.launch
-import java.util.concurrent.Executors
 
 fun <T, D : MutableLiveData<T>> D.doContent(data: T?) = this.postValue(data)
 fun <T, D : MutableLiveData<PackageData<T>>> D.content(data: T?) = this.postValue(dataContent(data))
@@ -25,44 +24,3 @@ fun <T> ViewModel.launch(liveData: MutableLiveData<PackageData<T>>,
 
 fun <T> dispatchException(liveData: MutableLiveData<PackageData<T>>): CoroutineExceptionHandler =
 		CoroutineExceptionHandler { _, throwable -> liveData.error(throwable) }
-
-class DataManager private constructor(threadNum: Int) {
-	companion object {
-		var instance: DataManager? = null
-			private set
-
-		@JvmStatic
-		fun init(threadNum: Int) {
-			if (instance != null) {
-				instance!!.shutdown()
-				instance = null
-			}
-			instance = DataManager(threadNum)
-		}
-
-		@JvmStatic
-		fun instance(): DataManager = instance!!
-	}
-
-	private val threadPool by lazy { Executors.newFixedThreadPool(threadNum) }
-
-	fun doRequest(action: () -> Unit) {
-		threadPool.execute {
-			action()
-		}
-	}
-
-	fun <T> doRequest(liveData: MutableLiveData<PackageData<T>>, action: () -> Unit) {
-		threadPool.execute {
-			try {
-				action()
-			} catch (e: Exception) {
-				liveData.error(e)
-			}
-		}
-	}
-
-	fun shutdown() {
-		threadPool.shutdown()
-	}
-}
