a7573b59869d4fdebf7f04124a80d1175e5018eb
Mystery0 <mystery0dyl520@gmail.com>
2020-04-12 19:47:29 +0800

    feat: 根据zhanghai的开源源码，添加对本机ip地址获取的方法

--- tools/src/main/java/vip/mystery0/tools/utils/NetworkTools.kt
+++ tools/src/main/java/vip/mystery0/tools/utils/NetworkTools.kt
@@ -1,14 +1,20 @@
 package vip.mystery0.tools.utils
 
-import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
 import androidx.annotation.RequiresPermission
-import vip.mystery0.tools.context
+import vip.mystery0.tools.doByTry
+import vip.mystery0.tools.getSystemService
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.net.UnknownHostException
+import java.util.*
+
 
 @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
 fun isConnectInternet(): Boolean {
-	val connectivityManager = context().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+	val connectivityManager = getSystemService(ConnectivityManager::class.java) ?: return false
 	if (sdkIsBefore(AndroidVersionCode.VERSION_Q)) {
 		val networkInfo = connectivityManager.activeNetworkInfo
 		return networkInfo != null && networkInfo.isConnected
@@ -23,4 +29,55 @@ fun isConnectInternet(): Boolean {
 		}
 	}
 	return false
+}
+
+@RequiresPermission(android.Manifest.permission.ACCESS_WIFI_STATE)
+fun getLocalInetAddress(): InetAddress? {
+	// WifiManagerPotentialLeak
+	val wifiManager: WifiManager = getSystemService(WifiManager::class.java) ?: return null
+	val wifiInfo = wifiManager.connectionInfo
+	if (wifiInfo != null) {
+		// WifiStateMachine doesn't support IPv6 as of P, so no need to get the original
+		// InetAddress object with reflection.
+		val addressInt = wifiInfo.ipAddress
+		if (addressInt != 0) {
+			return intToInetAddress(addressInt)
+		}
+	}
+	doByTry {
+		val networkInterfaces: Enumeration<NetworkInterface> = NetworkInterface.getNetworkInterfaces()
+		while (networkInterfaces.hasMoreElements()) {
+			val networkInterface: NetworkInterface = networkInterfaces.nextElement()
+			if (!networkInterface.isUp || networkInterface.isLoopback) {
+				continue
+			}
+			val inetAddresses: Enumeration<InetAddress> = networkInterface.inetAddresses
+			while (inetAddresses.hasMoreElements()) {
+				val inetAddress: InetAddress = inetAddresses.nextElement()
+				// Works for consumer IPv4 addresses.
+				if (!inetAddress.isSiteLocalAddress) {
+					continue
+				}
+				return@doByTry inetAddress
+			}
+		}
+	}
+	return null
+}
+
+/*
+     * @see android.net.NetworkUtils#intToInetAddress(int)
+     */
+private fun intToInetAddress(hostAddress: Int): InetAddress {
+	val addressBytes = byteArrayOf(
+			(0xff and hostAddress).toByte(),
+			(0xff and (hostAddress shr 8)).toByte(),
+			(0xff and (hostAddress shr 16)).toByte(),
+			(0xff and (hostAddress shr 24)).toByte()
+	)
+	return try {
+		InetAddress.getByAddress(addressBytes)
+	} catch (e: UnknownHostException) {
+		throw AssertionError()
+	}
 }
