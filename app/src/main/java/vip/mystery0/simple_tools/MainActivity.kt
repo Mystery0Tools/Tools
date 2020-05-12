fc687c26b12cd15086796b281974e67f9021417f
Mystery0 <mystery0dyl520@gmail.com>
2020-05-12 22:58:19 +0800

    release: 2.2.6

--- app/src/main/java/vip/mystery0/simple_tools/MainActivity.kt
+++ app/src/main/java/vip/mystery0/simple_tools/MainActivity.kt
@@ -1,17 +1,17 @@
 package vip.mystery0.simple_tools
 
 import android.util.Log
-import vip.mystery0.tools.ToolsClient
 import vip.mystery0.tools.base.BaseActivity
 import vip.mystery0.tools.factory.getExtensionsFromMimeType
 import vip.mystery0.tools.factory.toJson
+import vip.mystery0.tools.initTools
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
 	private val TAG = "MainActivity"
 
 	override fun requestData() {
 		super.requestData()
-		ToolsClient.initWithContext(this)
+		initTools()
 		Log.i(TAG, "text/plain".getExtensionsFromMimeType().toJson())
 	}
 }
