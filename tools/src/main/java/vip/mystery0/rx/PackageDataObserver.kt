a87d546b32a099d97a88fa5985156b59fe7e7515
Mystery0 <mystery0dyl520@gmail.com>
2020-05-12 22:10:03 +0800

    feat: 数据状态扩展更多的状态

--- tools/src/main/java/vip/mystery0/rx/PackageDataObserver.kt
+++ tools/src/main/java/vip/mystery0/rx/PackageDataObserver.kt
@@ -6,9 +6,18 @@ interface PackageDataObserver<T> : Observer<PackageData<T>> {
 	override fun onChanged(tPackageData: PackageData<T>) {
 		when (tPackageData.status) {
 			Status.Loading -> loading()
-			Status.Content -> content(tPackageData.data)
-			Status.Empty -> empty(tPackageData.data)
-			Status.Error -> error(tPackageData.data, tPackageData.error)
+			Status.Content -> {
+				content(tPackageData.data)
+				tPackageData.data?.let { contentNoEmpty(it) }
+			}
+			Status.Empty -> {
+				empty(tPackageData.data)
+				empty()
+			}
+			Status.Error -> {
+				error(tPackageData.data, tPackageData.error)
+				tPackageData.data?.let { error(tPackageData.error) }
+			}
 		}
 	}
 
@@ -16,7 +25,13 @@ interface PackageDataObserver<T> : Observer<PackageData<T>> {
 
 	fun empty(data: T?) {}
 
+	fun empty() {}
+
 	fun content(data: T?) {}
+
+	fun contentNoEmpty(data: T) {}
+
+	fun error(e: Throwable?) {}
 
 	fun error(data: T?, e: Throwable?) {}
 }
